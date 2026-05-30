3a7869bca86473dca71f981ae392bd68d0a250f6
Luis Muñoz <mm24121@ues.edu.sv>
2026-05-30 09:26:34 -0600

    feat: agregar modelo de datos para Asistencia_clases

---
+++ src/models/Asistencia_clases.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Clone, Serialize, Deserialize, FromRow, Debug, PartialEq)]
+pub struct AsistenciaClases {
+    pub id_asistencia: Option<i32>,
+    pub id_miembro: i32,
+    pub id_clase: i32,
+    pub fecha_asistencia: NaiveDate,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct NuevaAsistenciaClases {
+    pub id_miembro: i32,
+    pub id_clase: i32,
+    pub fecha_asistencia: Option<NaiveDate>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct ActualizarAsistenciaClases {
+    pub id_miembro: i32,
+    pub id_clase: i32,
+    pub fecha_asistencia: NaiveDate,
+}
